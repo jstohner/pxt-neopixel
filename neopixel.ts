a277b845c54638c48cc13f3df0a8e5201fc59837
jstohner <jan.stohner@telekom.de>
2024-12-30 11:45:04 +0100

    New mode GBR.

--- neopixel.ts
+++ neopixel.ts
@@ -33,7 +33,9 @@ enum NeoPixelMode {
     //% block="RGB+W"
     RGBW = 2,
     //% block="RGB (RGB format)"
-    RGB_RGB = 3
+    RGB_RGB = 3,
+    //% block="RGB (GBR format)"
+    RGB_GBR = 4
 }
 
 /**
@@ -401,14 +403,20 @@ namespace neopixel {
         }
 
         private setBufferRGB(offset: number, red: number, green: number, blue: number): void {
-            if (this._mode === NeoPixelMode.RGB_RGB) {
-                this.buf[offset + 0] = red;
-                this.buf[offset + 1] = green;
-            } else {
-                this.buf[offset + 0] = green;
-                this.buf[offset + 1] = red;
-            }
-            this.buf[offset + 2] = blue;
+	    if (this._mode !== NeoPixelMode.RGB_GBR) {
+                if (this._mode === NeoPixelMode.RGB_RGB) {
+                    this.buf[offset + 0] = red;
+                    this.buf[offset + 1] = green;
+                } else {
+                    this.buf[offset + 0] = green;
+                    this.buf[offset + 1] = red;
+                }
+                this.buf[offset + 2] = blue;
+	    else {
+                 this.buf[offset + 0] = green;
+                 this.buf[offset + 1] = blue;
+                 this.buf[offset + 2] = red;
+	    }
         }
 
         private setAllRGB(rgb: number) {
